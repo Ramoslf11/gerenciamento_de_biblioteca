80f5bc3fef3c15a360f0b2ea2eb63fd25a7d4a49
08ramoslf11 <157762398+08ramoslf11@users.noreply.github.com>
2025-09-18 21:15:30 -0300

    implementação de 2 menus (menu usuario / menu adm)

--- biblioteca.py
+++ biblioteca.py
@@ -63,27 +63,76 @@ def cadastro_livro():
         "titulo": input("Digite o título do livro: "),
         "autor": input("Digite o autor: "),
         "categoria": input("Digite a categoria: "),
-        # "disponivel": 
+        "disponivel": True
     }
     livros.append(livro)
     print(Fore.GREEN + f"\nLivro '{livro['titulo']}' cadastrado com sucesso!\n")
-    
+
+def Acervo_livros():
+    if not livros:
+        print(Fore.RED + "Nenhum livro cadastrado. Por favor, cadastre um livro primeiro.\n")
+        return
+    for livro in livros:
+        situação = "Disponível" if livro["disponivel"] else "Indisponível"
+        print(Fore.BLUE + f"ISBN: {livro['isbn']}, Título: {livro['titulo']}, Autor: {livro['autor']}, Categoria: {livro['categoria']}, Status: {situação}")
+
 tt = pyfiglet.figlet_format("\n=== Biblioteca Digital ===\n" )
 print(Fore.BLUE + Style.BRIGHT + tt)
 # Menu principal
 while True:
     print(Fore.CYAN + "Menu\n")
-    print(Fore.GREEN + "1 - Login")
-    print(Fore.YELLOW +"2 - Cadastro de Usuário")
-    print(Fore.RED + "3 - Sair")
-    opc = input(Fore.MAGENTA + "O que deseja fazer: ")
+    print(Fore.CYAN + "1 - Login")
+    print(Fore.CYAN +"2 - Cadastro de Usuário")
+    print(Fore.CYAN + "3 - Sair")
+    opc = input(Fore.CYAN + "O que deseja fazer: ")
 
     if opc == "1":
         login()
+        for i in usuarios:
+            if i["tipo"].lower() == "adm":
+                while True:
+                    print(Fore.CYAN + "\nMenu do Administrador\n")
+                    print(Fore.CYAN + "1 - Cadastrar Livro")
+                    print(Fore.CYAN + "2 - Acervo de Livros")
+                    print(Fore.CYAN + "3 - Relatório de livros")
+                    print(Fore.CYAN + "4 - Logout")
+                    opc_adm = input(Fore.CYAN + "O que deseja fazer: ")
+                    if opc_adm == "1":
+                        cadastro_livro()
+                    elif opc_adm == "2":
+                        Acervo_livros()
+                    elif opc_adm == "3":    
+                        relatorio_livros()
+                    elif opc_adm == "4":
+                        sair()
+                    else:
+                        print("Escolha uma opção válida!!")
+                        break
+            elif i["tipo"].lower() == "user":
+                while True:
+                    print(Fore.CYAN + "\nMenu do Usuário\n")
+                    print(Fore.CYAN + "1 - Livros disponíveis")
+                    print(Fore.CYAN + "2 - Empréstimo de livro")
+                    print(Fore.CYAN + "3 - Meus empréstimos")
+                    print(Fore.CYAN + "4 - Devolver livro")
+                    print(Fore.CYAN + "5 - Logout")
+                    opc_user = input(Fore.CYAN + "O que deseja fazer: ")
+                    if opc_user == "1":
+                        buscar_livro()
+                    elif opc_user == "2":
+                        Acervo_livros()
+                    elif opc_user == "3":
+                        meus_emprestimos()
+                    elif opc_user == "4":
+                        devolver_livro()
+                    elif opc_user == "5":
+                        sair()
+                    else:
+                        print("Escolha uma opção válida!!")
+        break
     elif opc == "2":
         cadastro_usuario()
     elif opc == "3":
         sair()
     else:
         print("Escolha uma opção válida!!")
-
